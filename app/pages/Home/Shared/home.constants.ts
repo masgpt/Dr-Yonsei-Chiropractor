9de0d97f95a57bed5ec7c8a8b6dcab24e7f7cad6
drblisspark <drbliss@sbcglobal.net>
2026-02-12 17:50:45 -0800

    Most recent 3 reviews

--- app/pages/Home/Shared/home.constants.ts
+++ app/pages/Home/Shared/home.constants.ts
@@ -1,4 +1,8 @@
 export const reviews = [
+  { id: 26, initials: 'CL', name: 'Connie Lee' },
+  { id: 27, initials: 'JC', name: 'J_Eun C' },
+  { id: 28, initials: 'LM', name: 'Leo Miguel' },
+  { id: 29, initials: 'ND', name: 'Nina Doering' },
   { id: 1, initials: 'DH', name: 'Danbi H.' },
   { id: 2, initials: 'CJ', name: 'Christina J.' },
   { id: 3, initials: 'JP', name: 'Jenny P.' },
